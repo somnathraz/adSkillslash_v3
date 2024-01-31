14d67c4266c2c4dd9e2841868144fc024fc6d1b1
somnath khadanga <somnathkhadanga@192.168.1.11>
2024-01-31 20:54:08 +0530

    2 commit

--- src/components/Skills/CoursePage/Header/Header.js
+++ src/components/Skills/CoursePage/Header/Header.js
@@ -109,7 +109,14 @@ const Header = ({
         <div className="bg-white px-11 py-3 max-sm:px-0 rounded shadow flex flex-col w-full z-[1] absolute mt-28">
           <div className="absolute w-[413px]  max-sm:w-[313px] h-[290px] max-sm:h-[220px] top-[-120px] max-sm:top-[-100px] left-12 min-[1440px]:left-8 max-sm:left-7 max-sm:flex max-sm:justify-center">
             <Link href={link}>
-              <Image src={imgSrc} alt="headerImg" fill priority quality={40} />
+              <Image
+                src={imgSrc}
+                alt="headerImg"
+                fill
+                priority
+                quality={40}
+                sizes="(max-width: 768px) 100%, (max-width: 1200px) 50%, 33%"
+              />
               <MdOutlinePlayCircleOutline className="absolute z-10 text-7xl text-white left-[42%] top-[41%] cursor-pointer " />
             </Link>
           </div>
