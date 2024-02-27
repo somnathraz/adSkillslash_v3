cd4fc88483674a6aab92ab8b2a817a70e2f87f34
somnath khadanga <somnathkhadanga@192.168.1.35>
2024-02-27 18:39:46 +0530

    ng page ds

--- src/pages/selfpaced/[id].js
+++ src/pages/selfpaced/[id].js
@@ -20,49 +20,29 @@ import BottomPrice from "@/components/Skills/BottomPrice/BottomPrice";
 const DataSciencePage = ({ DataScienceCourseData }) => {
   const [showNigeriaForm, setShowNigeriaForm] = useState(false);
   const [actualPrice, setActualPrice] = useState(
-    DataScienceCourseData.data.header.USDActualPrice
+    DataScienceCourseData.data.header.actualPrice
   );
   const [offerPrice, setOfferPrice] = useState(
-    DataScienceCourseData.data.header.USDOfferPrice
+    DataScienceCourseData.data.header.offerPrice
   );
 
-  useEffect(() => {
-    const fetchLocation = async () => {
-      try {
-        const response = await fetch(
-          "https://ipinfo.io/json?token=97f2da5f5a6db5"
-        );
-        if (response.status === 429) {
-          throw new Error("Rate limit exceeded. Too many requests.");
-        }
-        if (!response.ok) {
-          throw new Error(
-            `Failed to fetch location: ${response.status} ${response.statusText}`
-          );
-        }
-        const data = await response.json();
-        console.log("API Response:", data);
-        const { country } = data;
-
-        if (country === "NG") {
-          setShowNigeriaForm(true);
-          setActualPrice(DataScienceCourseData.data.header.NigeriaActualPrice);
-          setOfferPrice(DataScienceCourseData.data.header.NigeriaOfferPrice);
-          console.log("User is in Nigeria. Prices updated.");
-        }
-        if (country === "IN") {
-          setShowNigeriaForm(false);
-          setActualPrice(DataScienceCourseData.data.header.actualPrice);
-          setOfferPrice(DataScienceCourseData.data.header.offerPrice);
-          console.log("User is in India. Prices updated.");
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    fetchLocation();
-  }, []);
+  // useEffect(() => {
+  //   if (country === "NG") {
+  //     setShowNigeriaForm(true);
+  //     setActualPrice(DataScienceCourseData.data.header.NigeriaActualPrice);
+  //     setOfferPrice(DataScienceCourseData.data.header.NigeriaOfferPrice);
+  //     console.log("User is in Nigeria. Prices updated.");
+  //   }
+  //   if (country === "IN") {
+  //     setShowNigeriaForm(false);
+  //     setActualPrice(DataScienceCourseData.data.header.actualPrice);
+  //     setOfferPrice(DataScienceCourseData.data.header.offerPrice);
+  //     console.log("User is in India. Prices updated.");
+  //   } else {
+  //     setActualPrice(DataScienceCourseData.data.header.USDActualPrice);
+  //     setOfferPrice(DataScienceCourseData.data.header.USDOfferPrice);
+  //   }
+  // }, []);
 
   return (
     <div>
@@ -194,9 +174,35 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }) {
   const DataScienceCourseData = getPostData(params.id);
+  const fetchLocation = async () => {
+    try {
+      const response = await fetch(
+        "https://ipinfo.io/json?token=0fac06a7890a4e"
+      );
+      if (response.status === 429) {
+        throw new Error("Rate limit exceeded. Too many requests.");
+      }
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch location: ${response.status} ${response.statusText}`
+        );
+      }
+      const data = await response.json();
+
+      const { country } = data;
+
+      return country;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const country = await fetchLocation();
+  console.log(country, "before");
   return {
     props: {
       DataScienceCourseData,
+      country,
     },
   };
 }
